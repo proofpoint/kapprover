e867ec5064ade10b5dfc4f2eaee79ea46d233cdb
John Gardiner Myers <jgmyers@proofpoint.com>
2017-11-17 14:14:35 -0800

    Refactor PodIP/Namespace extraction to csr package

--- pkg/csr/csr_test.go
+++ pkg/csr/csr_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"k8s.io/client-go/kubernetes"
+	"regexp"
 	"testing"
 
 	_ "github.com/proofpoint/kapprover/pkg/inspectors/minrsakeysize"
-	"regexp"
 )
 
 var (
@@ -83,4 +83,157 @@ func TestExtractNotCSR(t *testing.T) {
 	certificateRequest, message := csr.Extract(certificateRequestBytes)
 	assert.Nil(t, certificateRequest, "CSR bytes")
 	assert.Regexp(t, regexp.MustCompile("Request had invalid certificate request: .*"), message, "CSR extract message")
+}
+
+func TestGetPodIpAndNamespace(t *testing.T) {
+	for _, testcase := range []struct {
+		name            string
+		clusterDomain   string
+		setupRequest    func(request *x509.CertificateRequest)
+		expectPodIp     string
+		expectNamespace string
+		expectMessage   string
+	}{
+		{
+			name:          "CnHasO",
+			expectMessage: "Subject has more than one name component",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.ExtraNames = []pkix.AttributeTypeAndValue{
+					{
+						Type:  []int{2, 5, 4, 10},
+						Value: "SomeOrg",
+					},
+				}
+			},
+		},
+		{
+			name:          "CnHasTwoCN",
+			expectMessage: "Subject has more than one name component",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.ExtraNames = []pkix.AttributeTypeAndValue{
+					{
+						Type:  []int{2, 5, 4, 3},
+						Value: []string{"example.com"},
+					},
+					{
+						Type:  []int{2, 5, 4, 3},
+						Value: []string{"example.org"},
+					},
+				}
+			},
+		},
+		{
+			name:          "NotInClusterDomain",
+			expectMessage: "Subject \"172-1-2-3.somenamespace.pod.example.com\" is not in the pod.cluster.local domain",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2-3.somenamespace.pod.example.com"
+			},
+		},
+		{
+			name:          "NotAPodDomain",
+			expectMessage: "Subject \"172-1-2-3.somenamespace.svc.cluster.local\" is not in the pod.cluster.local domain",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2-3.somenamespace.svc.cluster.local"
+			},
+		},
+		{
+			name:          "ExtraDomainComponents",
+			expectMessage: "Subject \"172-1-2-3.somenamespace.extra.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2-3.somenamespace.extra.pod.cluster.local"
+			},
+		},
+		{
+			name:          "MissingNamespace",
+			expectMessage: "Subject \"172-1-2-3.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2-3.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpTooShort",
+			expectMessage: "Subject \"172-1-2.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpTooLong",
+			expectMessage: "Subject \"172-1-2-3-4.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-2-3-4.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpNumberTooLarge",
+			expectMessage: "Subject \"172-256-2-3.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-256-2-3.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpNonDigit",
+			expectMessage: "Subject \"172-1a-2-3.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1a-2-3.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpLeadingZero",
+			expectMessage: "Subject \"172-01-2-3.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-01-2-3.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:          "IpMissingByte",
+			expectMessage: "Subject \"172--2-3.somenamespace.pod.cluster.local\" is not a POD-format name",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172--2-3.somenamespace.pod.cluster.local"
+			},
+		},
+		{
+			name:            "Good",
+			expectPodIp:     "172.1.0.3",
+			expectNamespace: "somenamespace",
+		},
+		{
+			name:          "ConfiguredNotInClusterDomain",
+			clusterDomain: "example.com",
+			expectMessage: "Subject \"172-1-0-3.somenamespace.pod.cluster.local\" is not in the pod.example.com domain",
+		},
+		{
+			name:          "ConfiguredGood",
+			clusterDomain: "example.com",
+			setupRequest: func(request *x509.CertificateRequest) {
+				request.Subject.CommonName = "172-1-0-3.somenamespace.pod.example.com"
+			},
+			expectMessage:   "",
+			expectPodIp:     "172.1.0.3",
+			expectNamespace: "somenamespace",
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			clusterDomain := testcase.clusterDomain
+			if clusterDomain == "" {
+				clusterDomain = "cluster.local"
+			}
+			certificateRequestTemplate := x509.CertificateRequest{
+				Subject: pkix.Name{
+					CommonName: "172-1-0-3.somenamespace.pod.cluster.local",
+				},
+			}
+			if testcase.setupRequest != nil {
+				testcase.setupRequest(&certificateRequestTemplate)
+			}
+			certificateRequestTemplate.Subject.Names = make([]pkix.AttributeTypeAndValue, 0)
+			certificateRequestTemplate.Subject.Names = append(certificateRequestTemplate.Subject.Names, pkix.AttributeTypeAndValue{Type: []int{2, 5, 4, 3}, Value: certificateRequestTemplate.Subject.CommonName})
+			certificateRequestTemplate.Subject.Names = append(certificateRequestTemplate.Subject.Names, certificateRequestTemplate.Subject.ExtraNames...)
+
+			podIp, namespace, msg := csr.GetPodIpAndNamespace(clusterDomain, &certificateRequestTemplate)
+			assert.Equal(t, testcase.expectPodIp, podIp, "Namespace")
+			assert.Equal(t, testcase.expectNamespace, namespace, "Namespace")
+			assert.Equal(t, testcase.expectMessage, msg, "Message")
+		})
+	}
 }
